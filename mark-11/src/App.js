e79e76599bd8bd9098c066df6a6192351e1de610
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2021-08-11 19:32:32 +0530

    styled and added the functionality to tell whether the birth date is lucky or not

--- mark-11/src/App.js
+++ mark-11/src/App.js
@@ -1,31 +1,87 @@
-
 import './App.css';
+import {useState} from "react";
 
 function App()
-{
+{  
+  const[number, setNumber] = useState(0);
+  const[month, setMonth] = useState(0);
+  const[date, setDate] = useState(0);
+  const[year, setYear] = useState(0);
+  const[verdict, setVerdict] = useState(false);
+  const[messageToBeShownToTheUser, setMessageToBeShownToTheUser] = useState("");
+
+  function handleDateChange(e)
+  {
+    let date1 = new Date(e.target.value);
+    setMonth(date1.getMonth() + 1);
+    setDate(date1.getDate());
+    setYear(date1.getFullYear());
+  }
+  function handleNumberChange(e)
+  {
+    setNumber(e.target.value);
+  }
   function handleSubmit(e)
   {
-    e.preventDefault();
+    e.preventDefault();//This is to prevent the default behaviour of reloding the page
+    setVerdict(true);//This is to show the message container to the user
+    let totalSumOfDigits = 0;
+    let tempMonth = month;
+    let tempDate = date;
+    let tempYear = year;
+    while(tempMonth > 0)
+    {
+      totalSumOfDigits = totalSumOfDigits + (tempMonth % 10);
+      tempMonth = Math.floor(tempMonth / 10);
+    }
+    while(tempDate > 0)
+    {
+      totalSumOfDigits = totalSumOfDigits + (tempDate % 10);
+      tempDate = Math.floor(tempDate / 10);
+    }
+    while(tempYear > 0)
+    {
+      totalSumOfDigits = totalSumOfDigits + (tempYear % 10);
+      tempYear = Math.floor(tempYear / 10);
+    }
+    console.log("Total Sum is: " + totalSumOfDigits);
+    if(totalSumOfDigits % number === 0)
+    {
+      //console.log("Lucky number");
+      setMessageToBeShownToTheUser("Lucky Number");
+    }
+    else{
+      //console.log("unlucky number");
+      setMessageToBeShownToTheUser("Unlucky Number");
+    }
+    e.target.reset();
   }
   return (
     <div className="App">
       <div className = "container">
-        <header>Is Your Birthday Lucky?(Mark-11)</header>
+        <header class = "header">Is Your Birthday Lucky?(Mark-11)</header>
 
         <div className = "alert">
-          We are not storing your data..
+          <strong>Privacy Notice!</strong> We are not storing your data..
         </div>
 
-        <label for = "description">Enter your birthdate and 
+        <label for = "description" className = "label label-description">Enter your birthdate and 
         lucky number to continue...</label>
 
         <form onSubmit = {(e) => handleSubmit(e)}>
-          <label for = "birth-date">Select your birth date</label>
-          <input type = "date" className = "input"></input>
-          <label for = "lucky-number">Enter your lucky number</label>
-          <input type = "number" className = "input"></input>
+          <label for = "birth-date" className = "label">Select your birth date: </label>
+          <input type = "date" className = "input" onChange = {(e) => handleDateChange(e)} required></input>
+          <label for = "lucky-number" className = "label">Enter your lucky number: </label>
+          <input type = "number" className = "input" onChange = {(e) => handleNumberChange(e)} required></input>
           <button type = "submit">Check</button>
         </form>
+
+        {verdict && 
+          <div>
+            {messageToBeShownToTheUser}
+          </div>
+        }
+
         <footer class = "footer">
         <div class = "footer-header">Connect with me on!</div>
         <ul class = "footer-list">
